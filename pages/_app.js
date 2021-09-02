2aa0d291bd6171d3d4c8ff5c0f432f430b8a93cc
bobbycrooz <idrisloove@gmail.com>
2021-09-02 07:25:47 -0700

    added base component

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,9 @@ import 'tailwindcss/tailwind.css';
 function MyApp({ Component, pageProps }) {
   return (
     <main className="bg-gradient-to-r from-grad-one to-grad-two p-1 text-secondary w-full  min-h-screen " id="main">
-    
+    <div className="inner w-full h-full">
       <Component {...pageProps} />
+    </div>
     </main>
   );
 }
