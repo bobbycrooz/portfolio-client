61a0174a4546e67a3f3af4ea2f52f8eaaa314dfa
bobbycrooz <idrisloove@gmail.com>
2021-09-11 00:23:00 +0100

    added audio

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,13 +8,13 @@ export default function Header() {
   const [showNav, setShowNav] = useState(true);
 
   return (
-    <div className="navbar bg-primary z-10 sticky top-0 h-20 flex justify-between items-center shadow-md w-full  p-2">
+    <div className="navbar bg-primary z-10 sticky top-0 h-20 flex justify-between items-center shadow-md w-full p-2  ">
       <h1 className=" text-grad-one font-black font-logo text-3xl">Idris</h1>
 
-      <Menu className={showNav && 'active'}>
-        <RotateBG className="menu_bg rounded-lg">
-          <div className="menu_cover flex items-center justify-between bg-primary rounded-lg">
-            <div className="nav-links text-lg font-joe text-textLow font-bold  p-1 w-full flex items-center justify-around">
+      <Menu className={showNav && 'active overflow-hidden'}>
+        <RotateBG className="menu_bg overflow-hidden rounded-lg h-full">
+          <div className="menu_cover overflow-hidden h-full flex items-center justify-between bg-primary rounded-lg">
+            <div className="nav-links  text-lg font-joe text-textLow font-bold  p-1 w-full flex items-center justify-around">
               <Link href="/">
                 <a>About</a>
               </Link>
@@ -32,12 +32,13 @@ export default function Header() {
               </Link>
             </div>
 
-            <div className="menu_icon absolute right-0">
+            <div className="menu_icon visible  absolute right-0">
               <Hamburger
                 color="#C3C3C3"
                 direction="right"
                 onToggle={() => setShowNav(pre => !pre)}
-                size={20}
+                size={25}
+                toggled={showNav}
               />
             </div>
           </div>
@@ -49,32 +50,41 @@ export default function Header() {
 
 const Menu = styled.h1`
   width: 48px;
+  height: 100%;
   transition: all 0.2s linear;
-
-  /* * {
-    border: 1px solid red;
-  } */
+  /* border: 1px solid red; */
+  /* padding: 2px; */
+  visibility: hidden;
 
   &.active {
     width: 80%;
-    .menu_bg {
-      .menu_cover {
-        width: 100%;
-        .nav-links {
-          visibility: visible;
-          display: inline;
-        }
+    visibility: visible;
+
+    .nav-links {
+      display: flex;
+      visibility: visible;
+      opacity: 1;
+      /* border: 1px solid red; */
+
+      a {
+        /* border: 1px solid red; */
+        color: #fff;
       }
     }
   }
 
   .menu_bg {
-    padding: 1px;
-    .menu_cover {
-      width: 48px;
-      .nav-links {
-        display: none;
-      }
-    }
+    padding: 1.7px;
   }
+
+  .nav-links {
+    display: none;
+    opacity: 0.2;
+    width: calc(100% - 38px);
+    /* visibility: hidden; */
+  }
+
+  /* * {
+    border: 1px solid red;
+  } */
 `;
--- components/Title.tsx
+++ components/Title.tsx
@@ -1,10 +1,28 @@
+import React, { useState } from 'react';
+
 import styled from 'styled-components';
 import Button from './Button';
 import Glow from './Glow';
 import { useRouter } from 'next/router';
+import { SiAudiomack } from 'react-icons/si';
 
 export default function Title() {
   const { pathname } = useRouter();
+  const [audioOn, setAudioOn] = useState(false);
+  const audio = new Audio('./track.mp3');
+
+  function audioControl(ctrl) {
+    // const soundTrack = document.getElementById('faded');
+
+    if (ctrl) {
+      audio.pause();
+      audio.currentTime = 0;
+      setAudioOn(false);
+    } else {
+      audio.play();
+      setAudioOn(true);
+    }
+  }
 
   return (
     <Pane className="pane_title z-10 sticky bg-gradient-to-r from-grad-one to-grad-two h-20 w-full p-0">
@@ -15,7 +33,15 @@ export default function Title() {
           <Button mt={false} w="70px" h="30px">
             Add
           </Button>
-        ) : null}
+        ) : (
+          <>
+            <SiAudiomack
+              onClick={() => audioControl(audioOn)}
+              size={30}
+              color={audioOn ? '#f44369' : '#474747'}
+            />
+          </>
+        )}
       </div>
     </Pane>
   );
