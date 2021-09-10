8f2ce3372da287e78b4608d4ef8c99a5c1d6ab54
bobbycrooz <idrisloove@gmail.com>
2021-09-11 00:39:49 +0100

    ..updated audio...

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import styled from 'styled-components';
 import Button from './Button';
@@ -9,20 +9,13 @@ import { SiAudiomack } from 'react-icons/si';
 export default function Title() {
   const { pathname } = useRouter();
   const [audioOn, setAudioOn] = useState(false);
-  const audio = new Audio('./track.mp3');
+  const [audio] = useState(
+    typeof Audio !== 'undefined' && new Audio('./track.mp3')
+  );
 
-  function audioControl(ctrl) {
-    // const soundTrack = document.getElementById('faded');
-
-    if (ctrl) {
-      audio.pause();
-      audio.currentTime = 0;
-      setAudioOn(false);
-    } else {
-      audio.play();
-      setAudioOn(true);
-    }
-  }
+  useEffect(() => {
+    audioOn ? audio.play() : audio.pause();
+  }, [audioOn]);
 
   return (
     <Pane className="pane_title z-10 sticky bg-gradient-to-r from-grad-one to-grad-two h-20 w-full p-0">
@@ -36,7 +29,7 @@ export default function Title() {
         ) : (
           <>
             <SiAudiomack
-              onClick={() => audioControl(audioOn)}
+              onClick={() => setAudioOn(p => !p)}
               size={30}
               color={audioOn ? '#f44369' : '#474747'}
             />
